68425779081186606799ad6bd9d1fc580b1a6d4f
Zorka5 <pmujta@gmail.com>
2017-07-26 20:41:52 +0200

    next is php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-public class Hello {
-  public static void main(String[] args){
-    System.out.print("Hello World\n");
-    System.out.print("Hello World - pierszy test\n");
-
-  }
-}
+<?php
+    echo "To jest test";
+?>
