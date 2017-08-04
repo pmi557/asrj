3dccc144ada1348230e9bd3b0e28a7e5d3c0db17
pmi557 <pmujta@gmail.com>
2017-08-04 08:47:58 +0200

    Skasowanie php

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
-  public static void main(String[] args){
-    System.out.print("Hello World");
+  public static void main(String[] args)
+  {
+    System.out.print("Hello World\n");
+    System.out.print("Wczytwyanie danych...");
   }
 }
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo "To jest test\n";
-    echo "Jest git !\n";
-    echo "Pierwsze koty za płoty\n";
-    
-?>
