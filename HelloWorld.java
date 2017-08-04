6d302b71a83be934c400686d73da2f5fc7ecf2bf
pmi557 <pmujta@gmail.com>
2017-08-04 09:09:26 +0200

    Poprawki w hello :)

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,13 @@ public class HelloWorld {
   public static void main(String[] args)
   {
     String Imie;
-    System.out.print("Wczytwyanie danych...Podaj Imie: ");
+    String Nazwisko;
+    System.out.print("Wczytwyanie danych...\n");
+    System.out.print("Podaj Imie: ");
     Scanner odczyt = new Scanner(System.in);
     Imie = odczyt.nextLine();
-    System.out.print("Twoje imie to: " +Imie);
+    System.out.print("Podaj Nazwisko: ");
+    Nazwisko = odczyt.nextLine();
+    System.out.println("Twoje imie i nazwisko: " +Imie +" " +Nazwisko);
   }
 }
